20c5efbba7c899a9f408242f7c546976312205be
ntpt7921 <83776509+ntpt7921@users.noreply.github.com>
2022-10-24 18:28:22 +0700

    Add code to user_code.h

---
+++ Core/Inc/user_code.h
@@ -0,0 +1,47 @@
+/*
+ * user_code.h
+ *
+ *  Created on: Oct 24, 2022
+ *      Author: ntpt
+ */
+
+#ifndef INC_USER_CODE_H_
+#define INC_USER_CODE_H_
+
+#include "main.h"
+
+#define SEG7_PATTERN_0 SEG0_Pin | SEG1_Pin | SEG2_Pin | SEG3_Pin | SEG4_Pin | SEG5_Pin
+#define SEG7_PATTERN_1 SEG1_Pin | SEG2_Pin
+#define SEG7_PATTERN_2 SEG0_Pin | SEG1_Pin | SEG3_Pin | SEG4_Pin | SEG6_Pin
+#define SEG7_PATTERN_3 SEG0_Pin | SEG1_Pin | SEG2_Pin | SEG3_Pin | SEG6_Pin
+#define SEG7_PATTERN_4 SEG1_Pin | SEG2_Pin | SEG5_Pin | SEG6_Pin
+#define SEG7_PATTERN_5 SEG0_Pin | SEG2_Pin | SEG3_Pin | SEG5_Pin | SEG6_Pin
+#define SEG7_PATTERN_6 SEG0_Pin | SEG2_Pin | SEG3_Pin | SEG4_Pin | SEG5_Pin | SEG6_Pin
+#define SEG7_PATTERN_7 SEG0_Pin | SEG1_Pin | SEG2_Pin
+#define SEG7_PATTERN_8 SEG0_Pin | SEG1_Pin | SEG2_Pin | SEG3_Pin | SEG4_Pin | SEG5_Pin | SEG6_Pin
+#define SEG7_PATTERN_9 SEG0_Pin | SEG1_Pin | SEG2_Pin | SEG3_Pin | SEG5_Pin | SEG6_Pin
+// assume all seg pattern line is in the same port
+#define COMMON_SEG7_PATTERN_PORT SEG0_GPIO_Port
+
+#define CONTROL_LINE_0 EN0_Pin
+#define CONTROL_LINE_1 EN1_Pin
+// assume all control pin is in the same port
+#define COMMON_CONTROL_PORT EN0_GPIO_Port
+
+#define NUMBER_OF_LED 2
+
+extern int16_t seg7_pattern_list[] = {
+    SEG7_PATTERN_0, SEG7_PATTERN_1, SEG7_PATTERN_2, SEG7_PATTERN_3,
+    SEG7_PATTERN_4, SEG7_PATTERN_5, SEG7_PATTERN_6, SEG7_PATTERN_7,
+    SEG7_PATTERN_8, SEG7_PATTERN_9,
+};
+
+extern control_line_list[NUMBER_OF_LED] = {
+    CONTROL_LINE_0, CONTROL_LINE_1,
+};
+
+void set_output_pattern_7seg(int32_t value);
+void disable_7seg(int32_t value);
+void enable_7seg(int32_t value);
+
+#endif /* INC_USER_CODE_H_ */
