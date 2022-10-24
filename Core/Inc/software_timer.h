dee0caf212ccfb8c0d8c752224b4b0f6fcb78742
ntpt7921 <83776509+ntpt7921@users.noreply.github.com>
2022-10-24 20:20:19 +0700

    Add code for software timer

--- Core/Inc/software_timer.h
+++ Core/Inc/software_timer.h
@@ -8,6 +8,28 @@
 #ifndef INC_SOFTWARE_TIMER_H_
 #define INC_SOFTWARE_TIMER_H_
 
+#include "main.h"
 
+#define TIMER_TICK_DURATION_MS 10
+extern int TIMER_CYCLE;
+
+extern int timer0_counter;
+extern int timer0_flag;
+
+extern int timer1_counter;
+extern int timer1_flag;
+
+extern int timer2_counter;
+extern int timer2_flag;
+
+
+void setTimer0(int duration);
+void timer0_run();
+
+void setTimer1(int duration);
+void timer1_run();
+
+void setTimer2(int duration);
+void timer2_run();
 
 #endif /* INC_SOFTWARE_TIMER_H_ */
--- Core/Src/software_timer.c
+++ Core/Src/software_timer.c
@@ -5,4 +5,69 @@
  *      Author: ntpt
  */
 
+#include "software_timer.h"
+
+int TIMER_CYCLE = TIMER_TICK_DURATION_MS;
+
+int timer0_counter = 0;
+int timer0_flag = 0;
+
+int timer1_counter = 0;
+int timer1_flag = 0;
+
+int timer2_counter = 0;
+int timer2_flag = 0;
+
+
+void setTimer0(int duration)
+{
+    timer0_counter = duration / TIMER_CYCLE;
+    timer0_flag = RESET;
+}
+void timer0_run()
+{
+    if (timer0_counter > 0)
+    {
+        timer0_counter--;
+    }
+    else if (timer0_counter <= 0)
+    {
+        timer0_flag = SET;
+    }
+}
+
+void setTimer1(int duration)
+{
+    timer1_counter = duration / TIMER_CYCLE;
+    timer1_flag = RESET;
+}
+void timer1_run()
+{
+    if (timer1_counter > 1)
+    {
+        timer1_counter--;
+    }
+    else if (timer1_counter <= 1)
+    {
+        timer1_flag = SET;
+    }
+}
+
+void setTimer2(int duration)
+{
+    timer2_counter = duration / TIMER_CYCLE;
+    timer2_flag = RESET;
+}
+void timer2_run()
+{
+    if (timer2_counter > 2)
+    {
+        timer2_counter--;
+    }
+    else if (timer2_counter <= 2)
+    {
+        timer2_flag = SET;
+    }
+}
+
 
