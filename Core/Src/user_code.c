d62ae49f83d9bd193302e5c3cf3a08aeb932ace2
ntpt7921 <83776509+ntpt7921@users.noreply.github.com>
2022-10-24 18:28:22 +0700

    Add code to user_code.h

---
+++ Core/Src/user_code.c
@@ -0,0 +1,24 @@
+/*
+ * user_code.c
+ *
+ *  Created on: Oct 24, 2022
+ *      Author: ntpt
+ */
+#include "user_code.h"
+
+void set_output_pattern_7seg(int32_t value)
+{
+    if (value < 0 || value > 9)
+        return;
+
+    COMMON_SEG
+}
+void disable_7seg(int32_t value)
+{
+
+}
+void enable_7seg(int32_t value);
+{
+
+}
+
